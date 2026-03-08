f3f2e409a83247e18a20e3f6108a5ad22a360f7d
Asuma09 <172264557+Asuma09@users.noreply.github.com>
2026-03-08 17:05:21 +0900

    Fix Render deploy: move db:migrate from buildCommand to startCommand to avoid PG::ConnectionBad during build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,4 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+# db:migrate は Render の build 時に DB が未起動の場合があるため、startCommand に移動します。
